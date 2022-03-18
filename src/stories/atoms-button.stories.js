85dbd83cf60f778e39da2885f0cac6b48e49e527
Natsuki Kato <n.kato@4nonome.com>
2022-03-18 14:14:29 +0900

    preparation

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,6 @@
+import Item_Example from "../components/atoms/example/button.html";
+import "../components/atoms/example/button.scss";
+
+export default { title: "Atoms/Button" }
+
+export const button = () => Item_Example;
