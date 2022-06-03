df9ff14764c8c098def0540278e89e062e5481b6
Natsuki Kato <n.kato@4nonome.com>
2022-06-03 16:41:55 +0900

    messsageの作成

---
+++ src/stories/molecules-message.stories.js
@@ -0,0 +1,6 @@
+import Item_Message from "../components/molecules/message/message.html";
+import "../components/molecules/message/message.scss";
+
+export default { title: "Atoms/Example" }
+
+export const Example = () => Item_Message;
