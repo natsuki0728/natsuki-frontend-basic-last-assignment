246d0f26073dff7feaa9ec5efc78f5c865f1843a
Natsuki Kato <n.kato@4nonome.com>
2022-06-15 17:38:08 +0900

    newsandeventsmoreの作成

---
+++ src/stories/molecules-news-and-events-more.stories.js
@@ -0,0 +1,6 @@
+import Molecules_more from "../components/molecules/news-and-events-more/news-and-events-more.html";
+import "../components/molecules/news-and-events-more/news-and-events-more.scss";
+
+export default { title: "Molecules/more" }
+
+export const more = () => Molecules_more;
